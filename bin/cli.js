fb53c11e98f40726c6019f0bcdd97dfd09da2dd3
stkevintan <stkevintan@zju.edu.cn>
2017-12-30 14:52:56 +0800

    fix: -s paramter may have some bugs

--- bin/cli.js
+++ bin/cli.js
@@ -17,20 +17,26 @@ const params = {
 };
 
 for (let i = 0; i < args.length; i++) {
-  if (args[i] === params.skipDraft) options.skipDraft = true;
-  else
-    for (let key in params) {
-      if (params.hasOwnProperty(key) && params[key] === args[i]) {
-        options[key] = args[++i];
-        break;
-      }
+  if (args[i] === params.skipDraft) {
+    // if -s is the last item in args, v should be undefined.
+    const v = args[i + 1];
+    options.skipDraft = v !== "false";
+    // the value is not omit.
+    if (v === "true" || v === "false") i++;
+    continue;
+  }
+  for (let key in params) {
+    if (params.hasOwnProperty(key) && params[key] === args[i]) {
+      options[key] = args[++i];
+      break;
     }
+  }
 }
-
+// throw JSON.stringify(options);
 lunr(options)
   .then(() => {
     console.log("done.");
   })
   .catch(e => {
-    console.log("failed: ",e);
+    console.log("failed: ", e);
   });
